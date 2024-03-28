428fc501324415ed7a97c76af2588a32d35167fa
Rawier <alexam211@hotmail.com>
2024-03-27 19:38:35 -0600

    modify projets

--- pages/portfolio.tsx
+++ pages/portfolio.tsx
@@ -88,38 +88,6 @@ const Portfolio: NextPage = (): ReactElement => {
         <svg className="inline-block w-6 h-6 text-gray-800 align-middle dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
           <path fill-rule="evenodd" d="M8.64 4.737A7.97 7.97 0 0 1 12 4a7.997 7.997 0 0 1 6.933 4.006h-.738c-.65 0-1.177.25-1.177.9 0 .33 0 2.04-2.026 2.008-1.972 0-1.972-1.732-1.972-2.008 0-1.429-.787-1.65-1.752-1.923-.374-.105-.774-.218-1.166-.411-1.004-.497-1.347-1.183-1.461-1.835ZM6 4a10.06 10.06 0 0 0-2.812 3.27A9.956 9.956 0 0 0 2 12c0 5.289 4.106 9.619 9.304 9.976l.054.004a10.12 10.12 0 0 0 1.155.007h.002a10.024 10.024 0 0 0 1.5-.19 9.925 9.925 0 0 0 2.259-.754 10.041 10.041 0 0 0 4.987-5.263A9.917 9.917 0 0 0 22 12a10.025 10.025 0 0 0-.315-2.5A10.001 10.001 0 0 0 12 2a9.964 9.964 0 0 0-6 2Zm13.372 11.113a2.575 2.575 0 0 0-.75-.112h-.217A3.405 3.405 0 0 0 15 18.405v1.014a8.027 8.027 0 0 0 4.372-4.307ZM12.114 20H12A8 8 0 0 1 5.1 7.95c.95.541 1.421 1.537 1.835 2.415.209.441.403.853.637 1.162.54.712 1.063 1.019 1.591 1.328.52.305 1.047.613 1.6 1.316 1.44 1.825 1.419 4.366 1.35 5.828Z" clip-rule="evenodd"/>
         </svg>
-        <span className="ml-1 text-sm font-medium text-gray-800 dark:text-gray-200 hover:underline">Metacrawler 2</span>
-      </a>
-      </div>
-      <div>
-        <span className="mr-1 text-xs font-semibold text-gray-500 dark:text-gray-400">
-          Electron
-        </span>
-      </div>
-    </div>
-    <div className="px-4 py-2">
-      <p className="text-sm text-gray-600 dark:text-gray-400">
-        Metadata cleaner, copy and other, with beauty visuals and effects.
-      </p>
-      <p className="text-sm text-gray-600 dark:text-gray-400">
-        Project made in Electron, HTML 5 and CSS, compatible with Windows, MacOS and Linux.
-      </p>
-      <br />
-      <p className="text-xs text-yellow-500">
-        Mar 27, 2024
-      </p>
-    </div>
-  </div>
-</div>
-
-  <div className="flex items-center justify-center w-full p-2 leading-tight md:w-1/2 md:mx-4 md:p-4'">
-  <div className="w-full overflow-hidden bg-gray-100 border-2 rounded-lg shadow-lg cursor-pointer dark:bg-gray-800 dark:hover:border-violet-800 hover:border-yellow-500">
-    <div className="flex items-center justify-between px-4 py-2 bg-white dark:bg-gray-900">
-      <div>
-      <a href="https://nexdrak.surge.sh" target="_blank" rel="noopener noreferrer" className="inline-block">
-        <svg className="inline-block w-6 h-6 text-gray-800 align-middle dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
-          <path fill-rule="evenodd" d="M8.64 4.737A7.97 7.97 0 0 1 12 4a7.997 7.997 0 0 1 6.933 4.006h-.738c-.65 0-1.177.25-1.177.9 0 .33 0 2.04-2.026 2.008-1.972 0-1.972-1.732-1.972-2.008 0-1.429-.787-1.65-1.752-1.923-.374-.105-.774-.218-1.166-.411-1.004-.497-1.347-1.183-1.461-1.835ZM6 4a10.06 10.06 0 0 0-2.812 3.27A9.956 9.956 0 0 0 2 12c0 5.289 4.106 9.619 9.304 9.976l.054.004a10.12 10.12 0 0 0 1.155.007h.002a10.024 10.024 0 0 0 1.5-.19 9.925 9.925 0 0 0 2.259-.754 10.041 10.041 0 0 0 4.987-5.263A9.917 9.917 0 0 0 22 12a10.025 10.025 0 0 0-.315-2.5A10.001 10.001 0 0 0 12 2a9.964 9.964 0 0 0-6 2Zm13.372 11.113a2.575 2.575 0 0 0-.75-.112h-.217A3.405 3.405 0 0 0 15 18.405v1.014a8.027 8.027 0 0 0 4.372-4.307ZM12.114 20H12A8 8 0 0 1 5.1 7.95c.95.541 1.421 1.537 1.835 2.415.209.441.403.853.637 1.162.54.712 1.063 1.019 1.591 1.328.52.305 1.047.613 1.6 1.316 1.44 1.825 1.419 4.366 1.35 5.828Z" clip-rule="evenodd"/>
-        </svg>
         <span className="ml-1 text-sm font-medium text-gray-800 dark:text-gray-200 hover:underline">NexDrak - Artist Landing Page</span>
       </a>
       </div>
